31c408ccadcc16ddeea33d75f387704c79941176
Nawaf0511 <nawaf050065707@gmail.com>
2026-08-25 00:12:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.listen(PORT, () => console.log(`🌐 Web server is running on port ${PORT}`)
 // ==========================================
 // يفضل دائماً استخدام المتغيرات البيئية (Environment Variables) في Render لحماية التوكن
 const MAIN_BOT_TOKEN = process.env.TOKEN || 'ضع_توكن_البوت_الرئيسي_هنا';
-const GEMINI_API_KEY = process.env.GEMINI_KEY || 'AQ.Ab8RN6JOdYVy0LDg7VUeFHhd0eJ-wLjDPlK5eH1554_3C46nEQ';
+const GEMINI_API_KEY = 'AQ.Ab8RN6ILssuTOTEdQs8iJnvOLVclLrfaOHif1LI1zYMxIlcHhA';
 const PREFIX = '!';
 const OWNER_ID = '972244532542459954';
 const LOG_CHANNEL_ID = '1506610506843291649'; 
